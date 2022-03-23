09455ce72613075f528f2336a5206cf472a2e3b6
Radmacher <dev@radmacher.me>
2022-03-23 18:51:07 +0100

    Added new icons

---
+++ apps/web/icons/CarbonDataTable.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function CarbonDataTable(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 32 32" {...props}><path d="M8 18h4v2H8zm6 0h4v2h-4zm-6-4h4v2H8zm6 8h4v2h-4zm6-8h4v2h-4zm0 8h4v2h-4z"></path><path d="M27 3H5a2.002 2.002 0 0 0-2 2v22a2.002 2.002 0 0 0 2 2h22a2.002 2.002 0 0 0 2-2V5a2.002 2.002 0 0 0-2-2Zm0 2v4H5V5ZM5 27V11h22v16Z"></path></svg>
+  )
+}
+export default CarbonDataTable
--- apps/web/icons/index.tsx
+++ apps/web/icons/index.tsx
@@ -4,5 +4,7 @@ import HornIcon from './CarbonBullhorn';
 import ChatIcon from './CarbonChat';
 import SettingsIcon from './CarbonSettings';
 import BackIcon from './CarbonArrowLeft';
+import RightArrow from './CarbonArrowRight';
+import CalendarIcon from './CarbonDataTable';
 
-export {HomeIcon, TableIcon, HornIcon, ChatIcon, SettingsIcon, BackIcon};
+export { HomeIcon, TableIcon, HornIcon, ChatIcon, SettingsIcon, BackIcon, RightArrow, CalendarIcon };

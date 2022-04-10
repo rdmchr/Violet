0a6c27a936cd4fc175425b6cd13abfae81146f71
Radmacher <dev@radmacher.me>
2022-04-10 13:48:17 +0200

    Fixed text color on the loading screen

--- apps/web/components/loading.tsx
+++ apps/web/components/loading.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 function Loading({ settings = false, small = false }) {
     const router = useRouter();
@@ -9,7 +7,7 @@ function Loading({ settings = false, small = false }) {
         <div className="w-[100vw] h-[100vh] bg">
             <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max`}>
                 <img src="/KoiDesign.svg" alt="Koi" className="portrait:w-[90vw] landscape:h-[80vh]" />
-                <h1 className="text-center text-2xl">Loading</h1>
+                <h1 className="text-center text-2xl text">Loading</h1>
             </div>
         </div>
     )

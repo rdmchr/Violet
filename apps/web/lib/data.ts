1a9c2c8fa98115485b5c75189403bf49d9ca1fa3
Radmacher <dev@radmacher.me>
2022-04-16 01:03:37 +0200

    Fixed data loading

--- apps/web/lib/data.ts
+++ apps/web/lib/data.ts
@@ -9,6 +9,9 @@ export async function fetchTimetable(timestamp: string): Promise<Week> {
     const { uid } = auth.currentUser;
     const docSnap = await getDoc(doc(db, 'timetables', uid));
     const data = await docSnap.data() as Week[];
-    const timetable = JSON.parse(data[timestamp] as string);
+    var timetable = null
+    try {
+        timetable = JSON.parse(data[timestamp] as string);
+    } catch (error) { }
     return timetable;
 }

a0f9633ec28c8087c50705259284e97085db2f80
Radmacher <dev@radmacher.me>
2022-04-16 00:35:41 +0200

    Added Sentry

--- apps/metrics-server/src/index.ts
+++ apps/metrics-server/src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import indexRoute from './routes/index';
+import * as Sentry from "@sentry/node";
 
 const port = process.env.PORT || 5001;
 const corsUrl = process.env.CORS_URL || 'http://localhost:3000';
@@ -17,6 +18,23 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions))
 
+Sentry.init({
+    dsn: "https://b9072aaa3eaa436c8ab56ea5db84cf62@o1186656.ingest.sentry.io/6338423",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: false }),
+        // enable Express.js middleware tracing
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0,
+});
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(indexRoute);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => console.log(`Node.js server started on port ${port}.`));
